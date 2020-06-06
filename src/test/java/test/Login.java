6337886136c735711d0fde5ff761f5bb3a849b51
rittujittu <rittujittu3@gmail.com>
2020-06-06 16:50:38 -0400

    made the test case work

--- src/test/java/test/Login.java
+++ src/test/java/test/Login.java
@@ -3,19 +3,30 @@ package test;
 import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import pom.LoginValidation;
+import utils.Common;
 
 
-public class Login extends Base{
+public class Login{
 	
-	private static WebDriver driver;
 	
-	public Login() {
-		this.driver = super.driver;
-	}
-	
+	Common common=new Common();
+	WebDriver driver;
+	  @BeforeSuite
+	  public void beforeSuite() {
+		  common.setup("chrome", "http://localhost:3001");
+		 driver= common.getDriver();
+	  }
+
+	  @AfterSuite
+	  public void afterSuite() {
+		  common.quitBrowser();
+	  }
+
   @Test
   public void test() throws InterruptedException {
 	  LoginValidation Login =new LoginValidation(driver);
